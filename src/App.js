36f90aaf7c8b83b6b4463725f8d7b19db3735506
gregorec <gregorec.veronika@gmail.com>
2022-12-07 18:54:37 +0100

    Updated from previous React Redux app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <div className="wrap">
+        <h1>Counter: {counter}</h1>
+        <div className="flex">
+          <button onClick={() => dispatch(increment(7))}>+</button>
+          <button onClick={() => dispatch(decrement())}>-</button>
+        </div>
+        {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ""}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createStore } from "@reduxjs/toolkit";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// npm install @reduxjs/toolkit react-redux
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
